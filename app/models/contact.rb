3d90909c328f161cea90bce114af2ca1e2bb5b40
渡辺健一 <watanabe-kenichi@rouge.plala.or.jp>
2018-06-14 23:50:24 +0900

    Adjusting CSS

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  validates :name, length: { in: 1..8 }               
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }        
+  validates :content, length: { in: 1..150 }
+                      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
-  # root 'chat_rooms#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chat_rooms, only:[:index] do
     collection do
